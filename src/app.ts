2a618c99af6af0e1dfeb77d92b96f3ef58934b75
Hurjay Naguit <work.naguithurjay@gmail.com>
2026-09-16 12:22:52 +0800

    refactor: reorganize src folder structure

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import type { Express } from "express";
 
+import cors from "cors";
+
 import testRoutes from "./routes/test.routes.js";
 
 const app: Express = express();
 
 // MIDDLEWARES
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // client's URL
+    credentials: true,
+  }),
+);
 
 // ROUTES
 app.use("/api/test", testRoutes);
